77da202d6c4eadc81c34ce5660924a96eeceebd1
chenzelin <chenzelinmail@gmail.com>
2017-05-10 19:39:32 +0800

    Signed-off-by: chenzelin <chenzelinmail@gmail.com>

---
+++ GenModel.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+import gensim
+import jieba
+import os
+from time import time
+import re
+if __name__ == '__main__':
+    pattern = re.compile("[\S]*(\s)*[\S]*")
+    # this option is only can used in linux environment
+    # jieba.enable_parallel(2)
+    with open('corpus_small.txt', 'r', encoding='utf-8') as f:
+        t1 = time()
+        txt = f.read()
+        txt = txt.replace("<content>", " ")
+        txt = txt.replace("</content>", " ")
+        print(txt)
+        with open('result.txt', 'w', encoding='utf-8') as result_f:
+            result = jieba.cut(txt, cut_all=True)
+            print("time using: ", time() - t1)
+            # print("Full mode: ", "/".join(result))
+            result_txt = " ".join(result)
+            # for word in result:
+            #     if re.match(pattern, word)[1] is None and len(word) > 0:
+            result_f.write(result_txt)
+            #         print(word)
+        # model = gensim.models.Word2Vec(result_txt)
+        # model.save('model')
+        model = gensim.models.Word2Vec.load('model')
+        sim = model.similarity("周杰伦", "林")
+        print(sim)
+        print("time using: ", time() - t1)
